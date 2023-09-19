5b0530cbbe4b3e54dda541f148dcdf796c80e8f3
leosg10 <143298320+leosg10@users.noreply.github.com>
2023-09-19 10:15:28 -0300

    Update 1163_Duck.py

--- Beecrowd/1163_Duck.py
+++ Beecrowd/1163_Duck.py
@@ -4,22 +4,22 @@ import math
 
 while True:
     try:
-        h=float(input())
-        d1,d2=(int(i) for i in input().split())
-        tries=int(input())
+        h = float(input())
+        d1,d2 = (int(i) for i in input().split())
+        tries = int(input())
 
         g = 9.80665
         pi = 3.14159
 
         def med(v,ang,h):
-            vy=v*math.sin(ang*pi/180)
-            vx=v*math.cos(ang*pi/180)
+            vy = v*math.sin(ang*pi/180)
+            vx = v*math.cos(ang*pi/180)
             tempo = vy/g+math.sqrt((2*(h+vy**2/(2*g)))/g)
-            s_x=vx*tempo
+            s_x = vx*tempo
             return(s_x)
 
         def saida(d1,d2,v,ang,h):
-                x=med(v,ang,h)
+                x = med(v,ang,h)
                 if x <d1 or x>d2:
                     print('{:.5f} -> NUCK'.format(x))
                 else:
