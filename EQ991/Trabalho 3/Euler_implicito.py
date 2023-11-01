4e57e6242ef6223bc99ee9e0853305ec5d9c2c98
leosg10 <143298320+leosg10@users.noreply.github.com>
2023-10-31 21:31:20 -0300

    Update Euler_implicito.py

--- EQ991/Trabalho 3/Euler_implicito.py
+++ EQ991/Trabalho 3/Euler_implicito.py
@@ -13,13 +13,12 @@ k = 0.15
 
 # Valores para diferentes passos de integração
 EI_1 = main.Euler_implicito(40, C_a1, C_a2, C_a3, 1)     # Passo 1 minuto
-EI_2 = main.Euler_implicito(20, C_a1, C_a2, C_a3, 2)     # Passo 2 minutos
-EI_5 = main.Euler_implicito(8, C_a1, C_a2, C_a3, 5)      # Passo 5 minutos
-EI_10 = main.Euler_implicito(4, C_a1, C_a2, C_a3, 10)    # Passo 10 minutos
-
+EI_2 = main.Euler_implicito(40, C_a1, C_a2, C_a3, 2)     # Passo 2 minutos
+EI_5 = main.Euler_implicito(40, C_a1, C_a2, C_a3, 5)      # Passo 5 minutos
+EI_10 = main.Euler_implicito(40, C_a1, C_a2, C_a3, 10)    # Passo 10 minutos
 
 # Gráficos
-fig, axs = plt.subplots(2, 2)
+fig, axs = plt.subplots(2, 2, layout='constrained')
 axs[0, 0].plot([i for i in range(40)], EI_1, linewidth=2,
                marker='.', label=("$C_{A1}$", "$C_{A2}$", "$C_{A3}$"))
 axs[0, 0].set_title('\u0394 = 1 minuto', fontsize=10)
@@ -38,8 +37,8 @@ for ax in axs.flat:
     ax.set_xlabel('Tempo (minutos)', fontsize=9)
     ax.set_ylabel('Concentração ($kgmol/m^3$)', fontsize=9)
     ax.grid(True)
-    ax.legend(loc = 'upper right')
-    ax.set_xlim(0,)
+    ax.legend(loc='upper right')
+    ax.set_xlim(0)
 
-plt.tight_layout()
+# plt.tight_layout()
 plt.show()
